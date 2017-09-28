03728d08490b2f0174c8912dca63202595d357f4
Marc Magon <ged_marc@hotmail.com>
2017-09-28 03:56:08 +0200

    JUnit 5 updates, Pretty Checkboxes implementation and a lot more
    Signed-off-by: Marc Magon <ged_marc@hotmail.com>

--- test/za/co/mmagon/jwebswing/plugins/angularslimscroll/SlimScrollOptionsTest.java
+++ test/za/co/mmagon/jwebswing/plugins/angularslimscroll/SlimScrollOptionsTest.java
@@ -1,6 +1,6 @@
 package za.co.mmagon.jwebswing.plugins.angularslimscroll;
 
-import org.junit.Test;
+import org.junit.jupiter.api.Test;
 import za.co.mmagon.jwebswing.htmlbuilder.css.colours.ColourCSSImpl;
 
 /**
