196e6ff64b7c2ed29532b74bc9e0633897f50492
GedMarc <ged_marc@hotmail.com>
2019-02-09 11:52:25 +0200

    The Website API Brings out many things....

--- src/main/java/com/jwebmp/plugins/angularslimscroll/AngularSlimScrollPageConfigurator.java
+++ src/main/java/com/jwebmp/plugins/angularslimscroll/AngularSlimScrollPageConfigurator.java
@@ -87,7 +87,7 @@ public class AngularSlimScrollPageConfigurator
 	@Override
 	public Page configure(Page page)
 	{
-		if (!page.isConfigured())
+		if (!page.isConfigured() && enabled())
 		{
 			JQueryPageConfigurator.setRequired(true);
 			AngularPageConfigurator.setRequired(true);
