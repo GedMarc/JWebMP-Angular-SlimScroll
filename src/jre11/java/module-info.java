f0c32116669f102ebfaf1c0e5ced729842188f5b
GedMarc <ged_marc@hotmail.com>
2020-11-13 00:38:49 +0200

    module name java.validation to jakarta.validation

--- src/jre11/java/module-info.java
+++ src/jre11/java/module-info.java
@@ -2,7 +2,7 @@ module com.jwebmp.plugins.angularslimscroll {
 	exports com.jwebmp.plugins.angularslimscroll;
 
 	requires com.jwebmp.core;
-	requires java.validation;
+	requires jakarta.validation;
 	requires com.guicedee.guicedinjection;
 	requires com.jwebmp.core.angularjs;
 
