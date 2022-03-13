84d3c8237b44c09cdccd5a5c4266fb4593c894c4
gedmarc <marc.magon@gmail.com>
2022-03-13 05:30:25 +0200

    Cleanup on angular and bootstrap for post-builds

--- src/jre11/java/module-info.java
+++ src/jre11/java/module-info.java
@@ -6,7 +6,7 @@ module com.jwebmp.plugins.angularslimscroll {
 	requires com.jwebmp.core;
 	requires jakarta.validation;
 	requires com.guicedee.guicedinjection;
-	requires com.jwebmp.core.angularjs;
+	requires com.jwebmp.core.angular;
 
 	provides com.jwebmp.core.services.IPageConfigurator with com.jwebmp.plugins.angularslimscroll.AngularSlimScrollPageConfigurator;
 	provides com.jwebmp.core.base.angular.services.IAngularModule with com.jwebmp.plugins.angularslimscroll.AngularSlimScrollModule;
--- src/main/java/com/jwebmp/plugins/angularslimscroll/AngularSlimScrollPageConfigurator.java
+++ src/main/java/com/jwebmp/plugins/angularslimscroll/AngularSlimScrollPageConfigurator.java
@@ -95,8 +95,8 @@ public class AngularSlimScrollPageConfigurator
 	{
 		if (!page.isConfigured() && enabled())
 		{
-			JQueryPageConfigurator.setRequired(true);
-			AngularPageConfigurator.setRequired(true);
+			
+			
 
 			page.getBody()
 			    .addJavaScriptReference(AngularSlimScrollReferencePool.SlimScrollReference.getJavaScriptReference());
